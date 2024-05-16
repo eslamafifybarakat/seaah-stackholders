05651b09204bde37c946f62f4dc89b319f9da527
eslamfront55 <eslamfront55@gmail.com>
2024-05-16 14:57:39 +0300

    Language Selector Theme

--- src/app/shared/components/language-selector/language-selector.component.ts
+++ src/app/shared/components/language-selector/language-selector.component.ts
@@ -1,6 +1,6 @@
 import { TranslationService } from '../../../services/generic/translation.service';
 import { CommonModule, isPlatformBrowser, NgOptimizedImage } from '@angular/common';
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, Input, PLATFORM_ID } from '@angular/core';
 import { keys } from '../../configs/localstorage-key';
 
 @Component({
@@ -15,6 +15,7 @@ export class LanguageSelectorComponent {
   language: string = '';
   page: string = '';
   collapse: boolean = false;
+  @Input() isLight:boolean = false;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
