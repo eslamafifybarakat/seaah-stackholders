198bd9f5206e5adf797d04a188c6e9d0fff8cfaa
eslamfront55 <eslamfront55@gmail.com>
2024-05-23 08:55:39 +0200

    auth slider

--- src/app/shared/carousels/auth-carousel/auth-carousel.ts
+++ src/app/shared/carousels/auth-carousel/auth-carousel.ts
@@ -1,11 +1,44 @@
 export const authCarouselEn = [
-  { id: 1, imgUrl: '../../../assets/images/auth/slider/img1.svg', title: 'Choose from a wide range of workforce', description: 'Enjoy the ability to choose exceptional workforce from over 20 different countries and more than 15 different job positions to get the right worker for your needs' },
-  { id: 1, imgUrl: '../../../assets/images/auth/slider/img1.svg', title: 'Choose from a wide range of workforce', description: 'Enjoy the ability to choose exceptional workforce from over 20 different countries and more than 15 different job positions to get the right worker for your needs' },
-  { id: 1, imgUrl: '../../../assets/images/auth/slider/img1.svg', title: 'Choose from a wide range of workforce', description: 'Enjoy the ability to choose exceptional workforce from over 20 different countries and more than 15 different job positions to get the right worker for your needs' }
+  {
+    "id": 1,
+    "imgUrl": "../../../assets/images/auth/slider/img1.svg",
+    "title": "Welcome to Sa'a",
+    "description": "We provide you with quick and smooth financing solutions to continue investing in your children's education with complete flexibility"
+  },
+  {
+    "id": 1,
+    "imgUrl": "../../../assets/images/auth/slider/img2.svg",
+    "title": "Interest-Free Installments",
+    "description": "At Sa'a, we strive to stand by you and assist you by offering interest-free school fee installments"
+  },
+  {
+    "id": 1,
+    "imgUrl": "../../../assets/images/auth/slider/img3.svg",
+    "title": "Easy Application Submission",
+    "description": "Easily submit a financing request on the platform"
+  }
 ]
 
+
 export const authCarouselAr = [
-  { id: 1, imgUrl: '../../../assets/images/auth/slider/img1.svg', title: 'اختر من بين مجموعة كبيرة من العمالة', description: 'تمتع بالقدرة على اختيار العمالة المميزة من بين أكثر من 20 دولة مختلفة وأكثر من 15 وظيفة مختلفة لتحصل على العامل المناسب لإحتياجاتك' },
-  { id: 1, imgUrl: '../../../assets/images/auth/slider/img1.svg', title: 'اختر من بين مجموعة كبيرة من العمالة', description: 'تمتع بالقدرة على اختيار العمالة المميزة من بين أكثر من 20 دولة مختلفة وأكثر من 15 وظيفة مختلفة لتحصل على العامل المناسب لإحتياجاتك' },
-  { id: 1, imgUrl: '../../../assets/images/auth/slider/img1.svg', title: 'اختر من بين مجموعة كبيرة من العمالة', description: 'تمتع بالقدرة على اختيار العمالة المميزة من بين أكثر من 20 دولة مختلفة وأكثر من 15 وظيفة مختلفة لتحصل على العامل المناسب لإحتياجاتك' }
+  {
+    id: 1,
+    imgUrl: '../../../assets/images/auth/slider/img1.svg',
+    title: 'مرحبًا بك في سعة',
+    description: 'نوفر لك حلول تمويلية سريعة وسلسة لمواصلة استثمار أبنائك في التعليم بمرونة تامة'
+  },
+
+  {
+    id: 1,
+    imgUrl: '../../../assets/images/auth/slider/img2.svg',
+    title: 'التقسيط بدون فوائد',
+    description: 'في سعة نسعي بالوقوف جانبك ومساعدتك بتقسيط الرسوم المدرسية بدون فوائد'
+  },
+
+  {
+    id: 1,
+    imgUrl: '../../../assets/images/auth/slider/img3.svg',
+    title: 'سهولة تقديم الطلب',
+    description: 'سهولة تقديم طلب للمنصه بالتمويل المطلوب '
+  }
 ]
